40bc4091d092e223aa49b46341db1dfbbbe29872
Kyer Harris <Kyertaur@gmail.com>
2023-03-06 18:20:10 -0700

    Added a password field

--- login.js
+++ login.js
@@ -1,5 +1,14 @@
 function login() {
   const nameEl = document.querySelector("#name");
-  localStorage.setItem("userName", nameEl.value);
+  const passEl = document.querySelector("#password")
+  if(localStorage.getItem("userName") != nameEl.value){
+    localStorage.setItem("userName", nameEl.value);
+    localStorage.setItem("password", passEl.value);
+  }
+  else{
+    if(localStorage.getItem("password") != passEl){
+      
+    }
+  }
   window.location.href = "play.html";
 }
